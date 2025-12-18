42c62b94c25cf2d5b6f81c167d0802e48a1ab444
Tijn <leviathan35yt@gmail.com>
2025-12-18 01:15:19 +0100

    Added seperate folders and files for each different container. Setup container for the fastapi app and made it running in docker. Added the drawio diagram. Next step: transform the model into the new files and make it work together with the message broker

---
+++ services/api/src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent  # api/src
+
+# Serve static files (css, js, images)
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+
+@app.get("/")
+def home():
+    return FileResponse(BASE_DIR / "static" / "index.html")
+
+## First ddocker build
+# Docker run
